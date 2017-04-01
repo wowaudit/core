874b90f36cff95df859fde04c5672f78c4d04091
Shedi <emiel@vanlankveld.me>
2017-04-01 13:11:19 +0200

    Refactor scraper; removed redundancy and improve start-up speed and allocation process

--- api/scraper.py
+++ api/scraper.py
@@ -15,35 +15,36 @@ class Scraper(object):
         self.mode = mode
         self.ids = ids
         self.start_time = start_time
-        if self.mode == 'production_patreon':
-            data = execute_query('SELECT guilds.guild_id, guilds.name, guilds.region, guilds.realm, guilds.key_code, guilds.last_checked, users.user_id, users.name, users.role, users.weekly_snapshot ' + \
-                                 ', users.legendaries, users.realm, users.per_spec, users.tier_data, users.status, users.warcraftlogs, users.last_refresh FROM guilds, users WHERE guilds.guild_id = users.guild_id AND guilds.patreon = 1 ORDER BY guilds.last_checked ASC')
+        self.fetch_select(self.ids) if self.ids else self.allocate()
 
-        else:
-            data = execute_query('SELECT guilds.guild_id, guilds.name, guilds.region, guilds.realm, guilds.key_code, guilds.last_checked, users.user_id, users.name, users.role, users.weekly_snapshot ' + \
-                                 ', users.legendaries, users.realm, users.per_spec, users.tier_data, users.status, users.warcraftlogs, users.last_refresh FROM guilds, users WHERE guilds.guild_id = users.guild_id ORDER BY guilds.last_checked ASC')
+    def fetch_select(self,guilds):
+        data = execute_query('SELECT guilds.guild_id, guilds.name, guilds.region, guilds.realm, guilds.key_code, guilds.last_checked, users.user_id, users.name, users.role, users.weekly_snapshot ' + \
+                             ', users.legendaries, users.realm, users.per_spec, users.tier_data, users.status, users.warcraftlogs, users.last_refresh FROM guilds, users WHERE guilds.guild_id = users.guild_id AND users.guild_id IN ({0})'.format(','.join(guilds)))
+        self.store(data)
 
+    def allocate(self):
+        allocations = MAX_ALLOCATED if self.mode not in ['snapshot_EU','snapshot_US'] else 1000000
+        guild_data = [str(guild[0]) for guild in execute_query('SELECT guild_id FROM guilds WHERE patreon = {0} ORDER BY last_checked ASC LIMIT {1}'.format(1 if self.mode == 'production_patreon' else 0, allocations))]
+
+        if self.mode in ['production','production_patreon']:
+            execute_query('UPDATE guilds SET last_checked = {0} WHERE guild_id IN ({1})'.format((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds(),','.join(guild_data)))
+
+        self.fetch_select(guild_data)
+
+    def store(self,data):
         count = 0
         for member in data:
             key_code = member[4]
-            if len(self.guilds) >= MAX_ALLOCATED and self.mode == 'production' and not self.ids and key_code not in self.done: break
-
             if key_code not in self.done:
                 if self.mode == 'snapshot_EU':
-                    self.guilds.append(Guild(member,mode,'EU'))
+                    self.guilds.append(Guild(member,self.mode,'EU'))
                 elif self.mode == 'snapshot_US':
-                    self.guilds.append(Guild(member,mode,'US'))
+                    self.guilds.append(Guild(member,self.mode,'US'))
                 else:
-                    self.guilds.append(Guild(member,mode))
+                    self.guilds.append(Guild(member,self.mode))
                 self.done[key_code] = count
                 count += 1
             else: self.guilds[self.done[key_code]].add_member(member)
-
-        self.allocate()
-
-    def allocate(self):
-        if self.mode in ['production','production_patreon']:
-            execute_query('UPDATE guilds SET last_checked = {0} WHERE guild_id IN ({1})'.format((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds(),','.join([str(guild.guild_id) for guild in self.guilds])))
 
     def run(self):
         if self.mode in ['debug','production','production_patreon']:
@@ -57,61 +58,25 @@ class Scraper(object):
             self.check_warcraftlogs()
 
     def check_all(self):
-        count = 0
+        if self.ids: return self.check_select()
         for guild in self.guilds:
-            count += 1
-            if self.mode in ['production','production_patreon']:
-                if time.time() - self.start_time >= MAXIMUM_RUNTIME: return False
-                if self.ids:
-                    if str(guild.guild_id) in self.ids:
-                        try:
-                            guild.check()
-                            print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                                   guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                        except:
-                            print '[ERROR][{0}][Guild ID: {1}] - Encountered an error, did not refresh. Total time since last refresh is {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                                   guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                else:
-                    try:
-                        guild.check()
-                        print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                               guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                    except:
-                        print '[ERROR][{0}][Guild ID: {1}] - Encountered an error, did not refresh. Total time since last refresh is {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                               guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-
-            if self.mode in ['snapshot_US','snapshot_EU']:
-                if guild.region == self.mode.split('_')[1]:
-                    if self.ids:
-                        if str(guild.guild_id) in self.ids:
-                            try:
-                                guild.check()
-                                print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                                       guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                            except:
-                                print '[ERROR][{0}][Guild ID: {1}] - Encountered an error, did not refresh. Total time since last refresh is {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                                       guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                    else:
-                        try:
-                            guild.check()
-                            print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                                   guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                        except:
-                            print '[ERROR][{0}][Guild ID: {1}] - Encountered an error, did not refresh. Total time since last refresh is {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                                   guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-
-            if self.mode == 'debug':
-                if self.ids:
-                    if str(guild.guild_id) in self.ids:
-                        guild.check()
-                        print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                               guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-                else:
-                    guild.check()
-                    print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
-                           guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
-
+            self.check_single(guild)
         return True
+
+    def check_select(self):
+        for guild in self.guilds:
+            if str(guild.guild_id) in self.ids:
+                self.check_single(guild)
+        return True
+
+    def check_single(self,guild):
+        try:
+            guild.check()
+            print '[INFO] [{0}][Guild ID: {1}] - Finished refreshing. Total time since last refresh was {2} seconds'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
+                   guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0))
+        except Exception, e:
+            print '[ERROR][{0}][Guild ID: {1}] - Encountered an error, did not refresh. Total time since last refresh was {2} seconds. Error: {3}'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),
+                   guild.guild_id,round((datetime.datetime.now()-datetime.datetime(2017,1,1)).total_seconds()-guild.last_checked,0),repr(e))
 
     def check_warcraftlogs(self):
         count = 0
