fb6ff5254d0c47d8657f1665ad7cb7f3cbd8a24e
Shedi <emiel@vanlankveld.me>
2017-04-22 01:21:05 +0200

    Don't stop after 1 hour in Google Cloud

--- production_pending.py
+++ production_pending.py
@@ -4,13 +4,6 @@ from scraper import Scraper
 from dateutil import tz
 from execute_query import execute_query
 
-
-try:
-    guild_ids = str(sys.argv[1]).split(',')
-    print '[INFO] [{0}] - Not using all guild IDs, but only the following: {1}'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),', '.join(guild_ids))
-except Exception:
-    guild_ids = False
-
 start_time = time.time()
 keep_going = True
 while keep_going:
@@ -23,7 +16,6 @@ while keep_going:
         guilds = Scraper('production',guild_ids,start_time,'tornado')
         keep_going = guilds.run()
         execute_query('DELETE FROM pending_changes WHERE guild_id IN ({0})'.format(','.join(guild_ids)))
-    if time.time() - start_time > MAXIMUM_RUNTIME: keep_going = False
 
 print '[INFO] [{0}] - {1}. Aborting now.'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),'Reached the maximum runtime' if (time.time() - start_time >= MAXIMUM_RUNTIME) else 'Done')
 
