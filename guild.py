d663512c19e10d3c6b80ee5bf41c36eb3c29e0e3
Shedi <emiel@vanlankveld.me>
2017-04-22 00:07:08 +0200

    Work around for caching in GCS

--- guild.py
+++ guild.py
@@ -196,6 +196,9 @@ class Guild(object):
             write_csv(csvfile,self.name,self.realm,self.region,self.version_message,self.warning_message,self.csv_data,self.mode,self.guild_id)
         bucket = storage.Client().get_bucket('wowcsv')
         gcloud_path = bucket.blob('{0}.csv'.format(self.key_code))
+        gcloud_path.delete() # Work around to avoid caching
+        gcloud_path = bucket.blob('{0}.csv'.format(self.key_code))
+        gcloud_path.make_public()
         gcloud_path.upload_from_filename(filename='{0}{1}.csv'.format(PATH_TO_CSV,self.key_code))
 
     def update_warcraftlogs(self):
