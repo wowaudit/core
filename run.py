cd7d0e4a112e6340a19edc80b356ad98b7f3060e
Shedi <emiel@vanlankveld.me>
2017-04-22 02:04:33 +0200

    Fix instance types

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import socket
 instance = socket.gethostname().split('-')[0]
 
 if instance == 'production': import production.py
-elif instance == 'production_patreon': import production_patreon.py
+elif instance == 'productionpatreon': import production_patreon.py
+elif instance == 'productionpending': import production_pending.py
 elif instance == 'warcraftlogs': import warcraftlogs.py
 elif instance == 'debug': import test.py
