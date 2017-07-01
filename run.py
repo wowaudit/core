a08cfe1e2372e33e150437bbbe8cf425c049f323
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2017-07-01 15:01:41 +0200

    Enable new WCL instance type

--- run.py
+++ run.py
@@ -7,3 +7,4 @@ elif instance == 'productionpatreon': import production_patreon
 elif instance == 'productionpending': import production_pending
 elif instance == 'productionplatinum': import production_platinum
 elif instance == 'warcraftlogs': import warcraftlogs
+elif instance == 'warcraftlogspending': import warcraftlogs_pending
