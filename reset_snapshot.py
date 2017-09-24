670b69011847925e53fa893fd2f9c36a2bff3dc5
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2017-09-24 22:17:21 +0200

    Update weekly snapshot reset script

--- reset_snapshot.py
+++ reset_snapshot.py
@@ -5,30 +5,6 @@ from dateutil import tz
 from json import loads, dumps
 from execute_query import execute_query
 
-def reset_m_data(region,team_ids=False):
-    if region:
-        m_data = execute_query('SELECT id, warcraftlogs FROM characters WHERE team_id IN (SELECT id from teams WHERE guild_id IN (SELECT id FROM guilds WHERE region = \'{0}\'))'.format(region))
-    elif team_ids:
-        m_data = execute_query('SELECT id, warcraftlogs FROM characters WHERE team_id IN ({0})'.format(','.join(team_ids)))
-    else:
-        m_data = False
-    count = 0
-    if m_data:
-        base_spec_query = 'UPDATE characters SET warcraftlogs = CASE '
-        for character in m_data:
-            try: data = loads(character[1])
-            except: data = False
-            if data:
-                try: data['weekly_highest_m'] = 0
-                except: pass
-                base_spec_query += 'WHEN id = {0} THEN \'{1}\' '.format(character[0],dumps(data).replace("'","\\'"))
-                count += 1
-                if count > 1000:
-                    execute_query(base_spec_query + ' ELSE warcraftlogs END',False)
-                    base_spec_query = 'UPDATE characters SET warcraftlogs = CASE '
-                    count = 0
-        execute_query(base_spec_query + ' ELSE warcraftlogs END',False)
-
 try:
     region = str(sys.argv[1])
     if region not in ['EU','US','TW','KR']: raise Exception
@@ -45,6 +21,7 @@ except Exception:
 
 if team_ids:
     execute_query('UPDATE characters SET weekly_snapshot = \'\' WHERE team_id IN ({0})'.format(','.join(team_ids)))
+    execute_query('UPDATE characters SET raiderio_weekly = NULL WHERE team_id IN ({0})'.format(','.join(team_ids)))
     print '[INFO] [{0}] - Snapshot resetted successfully for teams {1}'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),','.join(team_ids)   )
     reset_m_data(False, team_ids)
 else:
@@ -62,5 +39,5 @@ else:
         sys.exit()
     else:
         execute_query('UPDATE characters SET weekly_snapshot = \'\' WHERE team_id IN (SELECT id from teams WHERE guild_id IN (SELECT id FROM guilds WHERE region = \'{0}\'))'.format(region))
+        execute_query('UPDATE characters SET raiderio_weekly = NULL WHERE team_id IN (SELECT id from teams WHERE guild_id IN (SELECT id FROM guilds WHERE region = \'{0}\'))'.format(region))
         print '[INFO] [{0}] - Snapshot resetted successfully of region {1}'.format(datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz(TIME_ZONE)).strftime('%d-%m %H:%M:%S'),region)
-        reset_m_data(region)
