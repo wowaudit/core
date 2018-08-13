148137ad6d4364bef7b87fb76543ef1b3d369f1b
emielvanlankveld <emielvanlankveld@voormedia.com>
2018-08-13 19:27:49 +0200

    Add WCL IDs.

--- lib/constants/pve_constants.rb
+++ lib/constants/pve_constants.rb
@@ -22,35 +22,35 @@ RAID_DIFFICULTIES = {
 VALID_RAIDS = [{
   'name' => 'Uldir', 'days' => [0, 1, 2, 3, 4, 5, 6], 'id' => 0, 'partition' => 1,
   'encounters' => [{
-    'id' => 0, 'name' => 'Taloc', 'raid_ids' => {
+    'id' => 2144, 'name' => 'Taloc', 'raid_ids' => {
       'raid_finder' => 12786, 'normal' => 12787, 'heroic' => 12788, 'mythic' => 12789
     }
   }, {
-    'id' => 0, 'name' => 'MOTHER', 'raid_ids' => {
+    'id' => 2141, 'name' => 'MOTHER', 'raid_ids' => {
       'raid_finder' => 12790, 'normal' => 12791, 'heroic' => 12792, 'mythic' => 12793
     }
   }, {
-    'id' => 0, 'name' => 'Fetid Devourer', 'raid_ids' => {
+    'id' => 2128, 'name' => 'Fetid Devourer', 'raid_ids' => {
       'raid_finder' => 12794, 'normal' => 12795, 'heroic' => 12796, 'mythic' => 12797
     }
   }, {
-    'id' => 0, 'name' => 'Zek\'voz, Herald of N\'zoth', 'raid_ids' => {
+    'id' => 2136, 'name' => 'Zek\'voz, Herald of N\'zoth', 'raid_ids' => {
       'raid_finder' => 12798, 'normal' => 12799, 'heroic' => 12800, 'mythic' => 12801
     }
   }, {
-    'id' => 0, 'name' => 'Vectis', 'raid_ids' => {
+    'id' => 2134, 'name' => 'Vectis', 'raid_ids' => {
       'raid_finder' => 12802, 'normal' => 12803, 'heroic' => 12804, 'mythic' => 12805
     }
   }, {
-    'id' => 0, 'name' => 'Zul, Reborn', 'raid_ids' => {
+    'id' => 2145, 'name' => 'Zul, Reborn', 'raid_ids' => {
       'raid_finder' => 12808, 'normal' => 12809, 'heroic' => 12810, 'mythic' => 12811
     }
   }, {
-    'id' => 0, 'name' => 'Mythrax the Unraveler', 'raid_ids' => {
+    'id' => 2135, 'name' => 'Mythrax the Unraveler', 'raid_ids' => {
       'raid_finder' => 12813, 'normal' => 12814, 'heroic' => 12815, 'mythic' => 12816
     }
   }, {
-    'id' => 0, 'name' => 'G\'huun', 'raid_ids' => {
+    'id' => 2122, 'name' => 'G\'huun', 'raid_ids' => {
       'raid_finder' => 12817, 'normal' => 12818, 'heroic' => 12819, 'mythic' => 12820
     }
   }]
