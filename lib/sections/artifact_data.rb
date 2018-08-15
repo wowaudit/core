2ab3aa89536824d40d9a77f55b7398a0a06b40a6
emielvanlankveld <emielvanlankveld@voormedia.com>
2018-08-15 20:44:38 +0200

    Still send 0 values for AP gains.

--- lib/sections/artifact_data.rb
+++ lib/sections/artifact_data.rb
@@ -4,6 +4,10 @@ module Audit
     def self.add(character, data)
       character.data['artifact_level'] = data['items']['neck']['azeriteItem']['azeriteLevel'] rescue 0
       character.data['artifact_experience'] = data['items']['neck']['azeriteItem']['azeriteExperience'] rescue 0
+
+      # For old spreadsheet versions
+      character.data['ap_this_week'] = 0
+      character.data['ap_obtained_total'] = 0
     end
   end
 end
