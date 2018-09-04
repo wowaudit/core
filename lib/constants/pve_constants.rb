8d9f5b4835080239f3f1e2234f5bd0309cf1a83f
emielvanlankveld <emielvanlankveld@voormedia.com>
2018-09-04 23:50:08 +0200

    Start fetching Uldir raid data for WCL.

--- lib/constants/pve_constants.rb
+++ lib/constants/pve_constants.rb
@@ -19,7 +19,7 @@ RAID_DIFFICULTIES = {
 }
 
 VALID_RAIDS = [{
-  'name' => 'Uldir', 'days' => [], 'id' => 0, 'partition' => 1,
+  'name' => 'Uldir', 'days' => [0, 1, 2, 3, 4, 5, 6], 'id' => 0, 'partition' => 1,
   'encounters' => [{
     'id' => 2144, 'name' => 'Taloc', 'raid_ids' => {
       'raid_finder' => 12786, 'normal' => 12787, 'heroic' => 12788, 'mythic' => 12789
