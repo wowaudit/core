1cce4eef8262794bdca1086b41d2dd8bae63e31b
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2020-01-01 18:38:27 +0100

    Deal with characters added during the first week of the new year.

--- lib/models/character/character_bnet.rb
+++ lib/models/character/character_bnet.rb
@@ -53,7 +53,7 @@ module Audit
         }
 
         # Update the previous week with M+ data
-        if details['snapshots'][Audit.previous_week_year].include? Audit.previous_week
+        if (details['snapshots'][Audit.previous_week_year] || {}).include? Audit.previous_week
           details['snapshots'][Audit.previous_week_year][Audit.previous_week]['m+'] =
             self.data['weekly_highest_m+']
         end
