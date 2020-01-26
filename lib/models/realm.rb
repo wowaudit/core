47f015772ac2a70fb5588235157b27ef86e2e193
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2020-01-26 18:59:35 +0100

    Don't break on empty results for the new M+ checker.

--- lib/models/realm.rb
+++ lib/models/realm.rb
@@ -27,6 +27,7 @@ module Audit
 
         runs_by_character = {}
         leaderboards.results.map(&:leading_groups).flatten.each do |group|
+          next unless group # EmptyResult will have a nil value here
           group.members.each do |member|
             (runs_by_character[member.profile.id] ||= []) << group.keystone_level
           end
