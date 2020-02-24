eea9dcaf5a0dfd2109e26fd5db49235d4fb98f7f
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2020-02-24 09:33:18 +0100

    Make Redis work with empty sets.

--- lib/utils/redis.rb
+++ lib/utils/redis.rb
@@ -1,11 +1,14 @@
 module Audit
   module Redis
     def self.get_characters(ids)
+      return {} unless ids.any?
       REDIS.mget(*ids.map{ |id| "character:#{id}" }).map.with_index{ |data, i| [ids[i], JSON.parse(data)] }.to_h
     end
 
     def self.update(characters)
-      REDIS.mset characters.map{ |character| ["character:#{character.key}", character.details.to_json] }.flatten
+      if characters.any?
+        REDIS.mset characters.map{ |character| ["character:#{character.key}", character.details.to_json] }.flatten
+      end
     end
   end
 end
