cf5e0c82d0b350e4448bdbc59fad0e367f039418
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2020-06-09 09:06:23 +0200

    Deal with professions without tiers.

--- lib/sections/profession_data.rb
+++ lib/sections/profession_data.rb
@@ -5,7 +5,7 @@ module Audit
         professions = @data[:professions]['primaries'].select{ |p| BASE_PROFESSIONS.keys.include?(p['profession']['id']) }
 
         professions.each_with_index do |profession, index|
-          tier = profession['tiers'].select{ |tier| BFA_PROFESSIONS.keys.include?(tier['tier']['id']) }.first
+          tier = (profession['tiers'] || []).select{ |tier| BFA_PROFESSIONS.keys.include?(tier['tier']['id']) }.first
           @character.data["profession_#{index + 1}"] = "#{profession['profession']['name']} (#{tier ? tier['skill_points'] : '0'})"
         end
 
