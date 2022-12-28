65ec452a0625c09208b1a064d28d7271e222cdc3
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2022-12-28 20:03:26 +0100

    Allow tracking characters without a faction.

--- lib/sections/basic_data.rb
+++ lib/sections/basic_data.rb
@@ -11,7 +11,7 @@ module Audit
       @character.data['character_id'] = @character.id
       @character.data['join_date'] = @character.created_at
       @character.data['class'] = CLASSES[@character.class_id || @data.dig('character_class', 'id')]
-      @character.data['faction'] = @data['faction']['name']
+      @character.data['faction'] = @data.dig('faction', 'name')
       @character.data['blizzard_last_modified'] = @data['last_login_timestamp']
       @character.data['gender'] = @data['gender']['name']
       @character.data['race'] = @data['race']['name']
