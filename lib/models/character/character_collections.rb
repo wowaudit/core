feb606034b142f7242b18d1df7fe17e65ed05519
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2023-01-08 13:27:51 +0100

    Don't only store PvP vault data at the end of a reset.

--- lib/models/character/character_collections.rb
+++ lib/models/character/character_collections.rb
@@ -7,24 +7,9 @@ module Audit
     def update_snapshots
       current_week = details['snapshots'][Audit.year][Audit.week] || {}
 
-      details['snapshots'][Audit.year][Audit.week] = current_week.merge({ 'vault' => 3.times.map do |i|
+      details['snapshots'][Audit.year][Audit.week] = current_week.merge({ 'vault' => 9.times.map do |i|
         [(i + 1).to_s, self.last_refresh["great_vault_slot_#{i + 1}"] || self.data["great_vault_slot_#{i + 1}"]]
       end.to_h })
-
-      if !current_week['wqs']&.nonzero?
-        details['snapshots'][Audit.year][Audit.week]['wqs'] = self.data['wqs_done_total']
-
-        # Update the previous week with M+ and Vault data
-        if (details['snapshots'][Audit.previous_week_year] || {}).include? Audit.previous_week
-          details['snapshots'][Audit.previous_week_year][Audit.previous_week]['m+'] ||=  self.data['weekly_highest_m+']
-
-          last_week_vault = details['snapshots'][Audit.previous_week_year][Audit.previous_week]['vault'] || { '1' => '-', '2' => '-', '3' => '-', '4' => '-', '5' => '-', '6' => '-' }
-
-          details['snapshots'][Audit.previous_week_year][Audit.previous_week]['vault'] = last_week_vault.merge(3.times.map do |i|
-            [(i + 7).to_s, self.last_refresh["great_vault_slot_#{i + 7}"] || self.data["great_vault_slot_#{i + 7}"]]
-          end.to_h)
-        end
-      end
     end
 
     def check_data_completeness(response)
