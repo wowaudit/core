b822cfd1a71f1983d49b8188641e6621429c1c99
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2023-12-05 11:08:31 +0100

    Some characters don't have a pvp rank.

--- lib/sections/classic_era/pvp_data.rb
+++ lib/sections/classic_era/pvp_data.rb
@@ -3,7 +3,7 @@ module Audit
     class PvPData < Data
       def add
         if @data[:pvp_summary]
-          @character.data['honor_level'] = [@data[:pvp_summary][:pvp_rank], 14].min
+          @character.data['honor_level'] = [@data[:pvp_summary][:pvp_rank] || 0, 14].min
           @character.data['honor_title'] = CLASSIC_ERA_HONOR_TITLES.dig(@data.dig(:faction, :name).downcase.to_sym, @character.data['honor_level'].to_i) || ""
           @character.data['honorable_kills'] = @data[:pvp_summary][:honorable_kills]
           @character.data['makgora_duels_won'] =  @data[:pvp_summary][:pvpFeatureData]&.find { |feature| feature[:key] == "makgora_kills" }&.dig(:intValue) || 0
