1d6dc6fb75e11eaeb8bdeb739339f657d1e642e5
Emiel van Lankveld <emiel@vanlankveld.me>
2026-03-07 22:46:32 +0100

    Check better.

--- lib/sections/live/quest_data.rb
+++ lib/sections/live/quest_data.rb
@@ -18,8 +18,8 @@ module Audit
         @data.dig(:completed_quests, :quests)&.lazy&.each do |quest|
           @character.data['unity_against_the_void_weekly'] = 'yes' if UNITY_WEEKLY_QUESTS.include? quest[:id]
           @character.data['legends_of_the_haranir_weekly'] = 'yes' if HARANIR_WEEKLY_QUESTS.include? quest[:id]
-          @character.data['weekly_abundance'] = 'yes' if quest[:id] == 89507
-          @character.data['stormarion_assault'] = 'yes' if quest[:id] == 94581
+          @character.data['weekly_abundance'] = 'yes' if ABUNDANCE_WEEKLY_QUESTS.include? quest[:id]
+          @character.data['stormarion_assault'] = 'yes' if STORMARION_WEEKLY_QUESTS.include? quest[:id]
           @character.data['saltherils_soiree'] = 'yes' if SALTHERIL_WEEKLY_QUESTS.include? quest[:id]
           raid_buff_amount += 1 if RAID_BUFF_IDS.include? quest[:id]
         end
