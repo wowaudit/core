694a9258c15370c96f77c4c8a269b74e9156c829
Emiel van Lankveld <emiel@vanlankveld.me>
2026-03-19 12:50:10 +0100

    Use new table.

--- lib/utils/scheduler.rb
+++ lib/utils/scheduler.rb
@@ -29,15 +29,24 @@ module Audit
 
     def self.schedule_work(worker)
       if worker.type.include?("keystones")
-        table = "realms"
-        teams = DB.fetch("SELECT id, last_refreshed_#{worker.type} FROM realms WHERE kind = 'live' " +
-                             "ORDER BY last_refreshed_#{worker.type} ASC LIMIT 10").to_a
+        all_realm_ids = Realm.where(game_version: 'live').map{ |realm| realm.id }
+        DB.run(
+          "INSERT INTO realm_refresh_statuses (realm_id) " \
+          "SELECT id FROM unnest(ARRAY[#{all_realm_ids}]::integer[]) AS id " \
+          "WHERE NOT EXISTS (" \
+          "SELECT 1 FROM realm_refresh_statuses rrs WHERE rrs.realm_id = id" \
+          ")"
+        )
+
+        table = "realm_refresh_statuses"
+        teams = DB.fetch("SELECT realm_id as id, last_refreshed_#{worker.type} FROM #{table} " +
+                             "ORDER BY last_refreshed_#{worker.type} ASC NULLS FIRST LIMIT 10").to_a
       else
         table = 'teams'
         teams = DB.fetch(
           "SELECT t.id, ((#{Audit.now.to_i} - COALESCE(t.last_refreshed_#{worker.base_type}, 0)) " \
           "* t.refresh_factor) + t.refresh_factor AS priority, " \
-          "t.last_refreshed_#{worker.base_type} AS last_refreshed FROM teams t " \
+          "t.last_refreshed_#{worker.base_type} AS last_refreshed FROM #{table} t " \
           "INNER JOIN guilds g ON g.id = t.owner_id " \
           "WHERE g.active = TRUE ORDER BY priority DESC LIMIT 5"
         ).to_a
