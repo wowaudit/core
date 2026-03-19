7789a27da993e3ccb710046915027f73735bbb35
Emiel van Lankveld <emiel@vanlankveld.me>
2026-03-19 13:54:46 +0100

    Fix when no characters were updated.

--- lib/utils/writer.rb
+++ lib/utils/writer.rb
@@ -15,7 +15,9 @@ module Audit
       end
 
       tracking_characters = results.select { |r| r.character.status == 'tracking' }
-      DB.run("UPDATE characters SET refreshed_at = NOW() WHERE id IN (#{tracking_characters.map{ |c| c.character.id }.join(',')})")
+      if tracking_characters.any?
+        DB.run("UPDATE characters SET refreshed_at = NOW() WHERE id IN (#{tracking_characters.map{ |c| c.character.id }.join(',')})")
+      end
     end
   end
 end
