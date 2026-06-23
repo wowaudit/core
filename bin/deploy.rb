462ffdf9cc424de0abdac8ae8e8f340bff9770c3
Emiel van Lankveld <emiel@vanlankveld.me>
2026-06-23 22:29:45 +0200

    Deploy to pro.

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -4,23 +4,23 @@ require_relative('../lib/core')
 
 system("kubectl config use-context do-nyc3-wowaudit-scrapers")
 
-system("kubectl delete deployments wcl-deployment-acc")
-system("kubectl delete deployments blizzard-deployment-acc")
-system("kubectl delete deployments keystones-deployment-acc")
-system("kubectl delete deployments historical-keystones-deployment-acc")
-system("kubectl delete deployments raiderio-deployment-acc")
+system("kubectl delete deployments wcl-deployment")
+system("kubectl delete deployments blizzard-deployment")
+system("kubectl delete deployments keystones-deployment")
+system("kubectl delete deployments historical-keystones-deployment")
+system("kubectl delete deployments raiderio-deployment")
 
 sleep 5
 Audit::Schedule.all.each(&:destroy)
 
-system("docker build . --platform linux/amd64 -t shedi/wowaudit-acc")
-system("docker push shedi/wowaudit-acc:latest")
+system("docker build . --platform linux/amd64 -t shedi/wowaudit")
+system("docker push shedi/wowaudit:latest")
 
 Audit::Schedule.all.each(&:destroy)
 
-system("kubectl apply -f config/deploy/blizzard-deployment-acc.yml")
-system("kubectl apply -f config/deploy/keystones-deployment-acc.yml")
-system("kubectl apply -f config/deploy/historical-keystones-deployment-acc.yml")
-system("kubectl apply -f config/deploy/wcl-deployment-acc.yml")
-system("kubectl apply -f config/deploy/raiderio-deployment-acc.yml")
+system("kubectl apply -f config/deploy/blizzard-deployment.yml")
+system("kubectl apply -f config/deploy/keystones-deployment.yml")
+system("kubectl apply -f config/deploy/historical-keystones-deployment.yml")
+system("kubectl apply -f config/deploy/wcl-deployment.yml")
+system("kubectl apply -f config/deploy/raiderio-deployment.yml")
 system("kubectl apply -f config/deploy/scraper-metrics.yml")
