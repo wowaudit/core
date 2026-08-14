67533ce1e938850b0490b41b6953cbbc781b5e0c
Emiel van Lankveld <emiel@vanlankveld.me>
2026-08-14 14:14:12 +0200

    Prevent skips.

--- lib/constants/core_constants.rb
+++ lib/constants/core_constants.rb
@@ -5,7 +5,7 @@ CURRENT_VERSION = {
   classic_anniversary: 203,
 }
 
-PREVENT_SKIP_TIMESTAMP = DateTime.parse("2026-08-14 10:00:00 +0200")
+PREVENT_SKIP_TIMESTAMP = DateTime.parse("2026-08-14 14:20:00 +0200")
 
 TIME_ZONE = 'Europe/Amsterdam'
 HOUR = (1.0 / 24)
