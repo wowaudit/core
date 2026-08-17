d4b32b32a0311db2f48f3c5917f2277271694a5c
Emiel van Lankveld <emiel@vanlankveld.me>
2026-08-18 00:38:56 +0200

    Validate character name.

--- lib/models/character.rb
+++ lib/models/character.rb
@@ -2,6 +2,14 @@ module Audit
   class Character < Sequel::Model
     attr_accessor :team_rank, :details, :role, :note
 
+    def name=(value)
+      if value&.match?(/[%$"]/)
+        raise ArgumentError, "Character name contains invalid characters: #{value.inspect}"
+      end
+
+      super
+    end
+
     def realm
       @realm ||= Realm.find_by(id: realm_id)
     end
